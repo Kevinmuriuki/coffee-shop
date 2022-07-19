793cd2ae26881fe4833f2df6701f93fa698d147b
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-07-19 21:53:48 +0300

    Add a PATCH endpoint for drinks

--- backend/src/api.py
+++ backend/src/api.py
@@ -95,7 +95,25 @@ def create_drink(payload):
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(payload):
+    req = request.get_json()
 
+    try:
+        req_recipe = req['recipe']
+        if isinstance(req_recipe, dict):
+            req_recipe = [req_recipe]
+
+        drink = Drink()
+        drink.title = req['title']
+        drink.recipe = json.dumps(req_recipe)  # convert object to a string
+        drink.insert()
+
+    except BaseException:
+        abort(400)
+
+    return jsonify({'success': True, 'drinks': [drink.long()]})
 
 '''
 @TODO implement endpoint
