dee3bade6cedbb73dbcfaecc52f187176fb7badc
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-07-19 22:21:58 +0300

    update variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-3qhgi85p.us.auth0.com', // the auth0 domain prefix
+    audience: 'coffee shop', // the audience set for the auth0 app
+    clientId: 'FX8o40YgtYsLEa80cONv8WuYL1twGyrK', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
