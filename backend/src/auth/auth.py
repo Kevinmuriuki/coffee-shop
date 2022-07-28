c2c02f72ee52d2167c0800e3d1dbbcbf1724c541
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-07-28 12:50:13 +0300

    Add oAuth updates

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'dev-3qhgi85p.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee'
+API_AUDIENCE = 'https://127.0.0.1:5000'
 
 ## AuthError Exception
 '''
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'dev-3qhgi85p.us', // the auth0 domain prefix
-    audience: 'coffee', // the audience set for the auth0 app
-    clientId: 't0NnAt39RTMbWX32XupmjLMvB1CIaxjm', // the client id generated for the auth0 app
+    audience: 'https://127.0.0.1:5000', // the audience set for the auth0 app
+    clientId: 'iqVNEV4d3aXlqKRVTSNQ2va2HSs9tKNJ', // the client id generated for the auth0 app
     callbackURL: 'https://127.0.0.1:4200', // the base url of the running ionic application. 
   }
 };
